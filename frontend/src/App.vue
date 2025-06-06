d3de05d67a31346ab07b7c63d78457631361546f
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-06-06 13:24:34 +0100

    feat: base commit, home page setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Home from './views/Home.vue';
+</script>
+
+<template>
+  <div class="flex items-center min-h-screen w-full">
+    <Home />
+  </div>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-full h-full">
+        <img src="https://images.ctfassets.net/nnkxuzam4k38/2SvDjcgyav5C1DOb79JKXl/d3b06db5bb6bdb4ab237f666b5b4980e/compute-ea4c57a4.png"
+            alt="background" class="w-full h-full object-cover absolute inset-0 -z-10 brightness-50">
+        <div class="flex flex-wrap items-center px-8 pt-4">
+            <span class="text-white text-3xl font-bold whitespace-nowrap">Welcome to my blog about software engineering.
+                I am
+                an expert in</span>
+            <div class="flex flex-wrap items-center gap-x-2 gap-y-2 ml-2">
+                <div
+                    class="bg-gray-800 bg-opacity-90 rounded-xl flex items-center justify-center transition-all hover:bg-gray-700 duration-300 w-16 h-16 min-w-16 max-w-16">
+                    <a href="https://vuejs.org/" target="_blank" class="w-full h-full flex items-center justify-center">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+                            alt="vue" class="h-10 object-contain">
+                    </a>
+                </div>
+                <span class="text-white text-3xl font-bold">,</span>
+                <div
+                    class="bg-gray-800 bg-opacity-90 rounded-xl flex items-center justify-center transition-all hover:bg-gray-700 duration-300 w-16 h-16 min-w-16 max-w-16">
+                    <a href="https://www.thymeleaf.org/" target="_blank"
+                        class="w-full h-full flex items-center justify-center">
+                        <img src="https://img.icons8.com/?size=512&id=iWpVsSkAqPpZ&format=png" alt="thymeleaf"
+                            class="h-10 object-contain">
+                    </a>
+                </div>
+                <span class="text-white text-3xl font-bold">and</span>
+                <div
+                    class="bg-gray-800 bg-opacity-90 rounded-xl flex items-center justify-center transition-all hover:bg-gray-700 duration-300 w-16 h-16 min-w-16 max-w-16">
+                    <a href="https://tailwindcss.com/" target="_blank"
+                        class="w-full h-full flex items-center justify-center">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png"
+                            alt="tailwind" class="h-10 object-contain">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
