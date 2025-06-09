ea48d36d2f60527b386eef4d9807d2d4ea717632
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-06-09 13:20:02 +0100

    feat: add favourite topics section

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -66,7 +66,7 @@
 
       <!-- Scroll button -->
       <button
-        @click="scrollToNextSection"
+        @click="scrollToNextSection('topics')"
         class="absolute bottom-14 left-1/2 -translate-x-1/2 transform rounded-full bg-white/10 p-4 text-white transition-all duration-300 hover:bg-white/20"
         aria-label="Scroll to next section"
       >
@@ -87,13 +87,52 @@
       </button>
       <!-- Invisible trigger for auto-scroll -->
       <div
-        ref="autoScrollTrigger"
+        ref="autoScrollTriggerHero"
+        class="pointer-events-none absolute bottom-0 left-0 h-2 w-full"
+      ></div>
+    </section>
+
+    <!-- the my favourite topics section -->
+    <section
+      id="topics"
+      class="relative min-h-screen w-full bg-gray-900 px-4 py-20"
+    >
+      <div class="mx-auto max-w-2xl px-0">
+        <h2 class="mb-8 text-center text-2xl font-bold text-white md:text-4xl">
+          My favourite topics
+        </h2>
+      </div>
+
+      <!-- button to scroll to the next section -->
+      <button
+        @click="scrollToNextSection('links')"
+        class="absolute bottom-10 left-1/2 -translate-x-1/2 transform rounded-full bg-white/10 p-4 text-white transition-all duration-300 hover:bg-white/20"
+        aria-label="Scroll to next section"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 14l-7 7m0 0l-7-7m7 7V3"
+          />
+        </svg>
+      </button>
+      <!-- Invisible trigger for auto-scroll to links -->
+      <div
+        ref="autoScrollTriggerTopics"
         class="pointer-events-none absolute bottom-0 left-0 h-2 w-full"
       ></div>
     </section>
 
     <!-- the external links section -->
-    <section id="links" class="min-h-screen w-full bg-gray-900 px-4 py-20">
+    <section id="links" class="min-h-screen w-full bg-black px-4 py-20">
       <div class="mx-auto max-w-2xl px-0">
         <h2 class="mb-8 text-center text-2xl font-bold text-white md:text-4xl">
           You can find me on the following platforms:
@@ -113,24 +152,26 @@
           >
         </div>
 
-        <span class="text-white text-center text-2xl font-bold mt-4 w-full block">
+        <span
+          class="mt-4 block w-full text-center text-2xl font-bold text-white"
+        >
           My articles on Dev.to
         </span>
 
         <div class="mt-4 flex flex-wrap justify-center gap-4">
           <a
             href="https://dev.to/bcostaaa01"
-            class="relative bg-opacity-90 flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-gray-800 transition-all duration-300 hover:scale-105 hover:border-2 hover:border-blue-500 hover:bg-gray-700 hover:shadow-lg group"
+            class="bg-opacity-90 group relative flex h-full w-full cursor-pointer items-center justify-center rounded-xl bg-gray-800 transition-all duration-300 hover:scale-105 hover:border-2 hover:border-blue-500 hover:bg-gray-700 hover:shadow-lg"
           >
             <img
               src="../assets/dev-to.png"
               alt="dev-to"
-              class="h-full object-contain rounded-xl shadow-md group-hover:brightness-50 transition-all duration-300"
+              class="h-full rounded-xl object-contain shadow-md transition-all duration-300 group-hover:brightness-50"
             />
             <div
-              class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white text-center px-4"
+              class="absolute inset-0 flex items-center justify-center px-4 text-center text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
             >
-              <span class="bg-black bg-opacity-70 p-2 rounded-lg">
+              <span class="bg-opacity-70 rounded-lg bg-black p-2">
                 Dev.to Profile
               </span>
             </div>
@@ -144,25 +185,26 @@
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-const autoScrollTrigger = ref<HTMLElement | null>(null);
-let observer: IntersectionObserver | null = null;
+const autoScrollTriggerHero = ref<HTMLElement | null>(null);
+const autoScrollTriggerTopics = ref<HTMLElement | null>(null);
+let observerHero: IntersectionObserver | null = null;
+let observerTopics: IntersectionObserver | null = null;
 
 // scroll to the links section
-const scrollToNextSection = () => {
-  // find the links section to scroll to
-  const nextSection = document.getElementById('links');
+const scrollToNextSection = (section: string) => {
+  const nextSection = document.getElementById(section);
   if (nextSection) {
     nextSection.scrollIntoView({ behavior: 'smooth' });
   }
 };
 
 onMounted(() => {
-  if (autoScrollTrigger.value) {
-    observer = new IntersectionObserver(
+  if (autoScrollTriggerHero.value) {
+    observerHero = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting) {
-          // when user reaches the bottom of the hero section, scroll to the links section
-          scrollToNextSection();
+          // when user reaches the bottom of the hero section, scroll to the topics section
+          scrollToNextSection('topics');
         }
       },
       {
@@ -170,13 +212,31 @@ onMounted(() => {
         threshold: 1.0,
       }
     );
-    observer.observe(autoScrollTrigger.value);
+    observerHero.observe(autoScrollTriggerHero.value);
+  }
+  if (autoScrollTriggerTopics.value) {
+    observerTopics = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          // when user reaches the bottom of the topics section, scroll to the links section
+          scrollToNextSection('links');
+        }
+      },
+      {
+        root: null,
+        threshold: 1.0,
+      }
+    );
+    observerTopics.observe(autoScrollTriggerTopics.value);
   }
 });
 
 onBeforeUnmount(() => {
-  if (observer && autoScrollTrigger.value) {
-    observer.unobserve(autoScrollTrigger.value);
+  if (observerHero && autoScrollTriggerHero.value) {
+    observerHero.unobserve(autoScrollTriggerHero.value);
+  }
+  if (observerTopics && autoScrollTriggerTopics.value) {
+    observerTopics.unobserve(autoScrollTriggerTopics.value);
   }
 });
 </script>
